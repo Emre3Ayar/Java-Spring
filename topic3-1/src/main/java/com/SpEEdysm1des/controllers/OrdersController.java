e19ba53d4b3d2b4ce4c6cb09ed5bacd53cf75a91
Emre3Ayar <73703934+Emre3Ayar@users.noreply.github.com>
2024-01-19 17:58:17 +0100

    Java Spring(topic3-1) Dependency In., Service
    on topic3-1 implemented Dependency Injection and Interfaces along with Services

--- topic3-1/src/main/java/com/SpEEdysm1des/controllers/OrdersController.java
+++ topic3-1/src/main/java/com/SpEEdysm1des/controllers/OrdersController.java
@@ -3,25 +3,32 @@ package com.SpEEdysm1des.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.SpEEdysm1des.models.OrderModel;
+import com.SpEEdysm1des.services.OrdersBusinessService;
 
 @Controller
 @RequestMapping("/orders")
 public class OrdersController {
+	
+	
+	OrdersBusinessService service;
+	@Autowired
+	public OrdersController(OrdersBusinessService service) {
+		super();
+		this.service = service;
+	}
+
 	@GetMapping("/")
 	public String showAllOrders(Model model)
-	{
-		List<OrderModel> orders = new ArrayList<OrderModel>();
-		orders.add(new OrderModel(0l, "000", "Sky diving experience", 1500.0f, 1));
-		orders.add(new OrderModel(1l, "001", "Cycling", 1120.0f, 2));
-		orders.add(new OrderModel(2l, "002", "Swimming", 500.0f, 3));
-		orders.add(new OrderModel(3l, "003", "Programming", 1760.0f, 4));
-		orders.add(new OrderModel(4l, "004", "Jumping", 2500.0f, 5));
+	{	
+		List<OrderModel> orders = service.getOrders();
+		
 		model.addAttribute("title", "Here is what I want to tdo");
 		model.addAttribute("orders", orders);
 		return "orders.html";
---
+++ topic3-1/src/main/java/com/SpEEdysm1des/services/OrdersBusinessServiceInterface.java
@@ -0,0 +1,13 @@
+package com.SpEEdysm1des.services;
+
+import java.util.List;
+
+import com.SpEEdysm1des.models.OrderModel;
+
+public interface OrdersBusinessServiceInterface {
+	public void test();
+	
+	public List<OrderModel> getOrders();
+	
+	
+}
