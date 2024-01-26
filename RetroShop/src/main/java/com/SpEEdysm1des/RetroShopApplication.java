0f4017bdc01564dbf480632c62d35ca69d93a084
Emre3Ayar <73703934+Emre3Ayar@users.noreply.github.com>
2024-01-26 20:46:42 +0100

    Retroshop Wedshop
    new updated from Gradle to Maven

--- RetroShop/src/main/java/com/SpEEdysm1des/RetroShopApplication.java
+++ RetroShop/src/main/java/com/SpEEdysm1des/RetroShopApplication.java
@@ -1,11 +1,9 @@
-package com.example.SpEEdysm1des;
+package com.SpEEdysm1des;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.SpEEdysm1des")
 public class RetroShopApplication {
 
 	public static void main(String[] args) {
--- RetroShop/src/test/java/com/SpEEdysm1des/RetroShopApplicationTests.java
+++ RetroShop/src/test/java/com/SpEEdysm1des/RetroShopApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.SpEEdysm1des;
+package com.SpEEdysm1des;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
